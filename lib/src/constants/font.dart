554033b9069be4613c03a9d2177f69f6f575ebc1
Đinh Văn Nam <itchange.nw@gmail.com>
2024-06-10 15:33:35 +0700

    Add Font

---
+++ lib/src/constants/font.dart
@@ -0,0 +1,31 @@
+class AppFont {
+  AppFont._();
+
+  // Font Roboto
+  static const String robotoBlack = "Roboto-Black";
+  static const String robotoBlackItalic = "Roboto-BlackItalic";
+  static const String robotoBold = "Roboto-Bold";
+  static const String robotoBoldItalic = "Roboto-BoldItalic";
+  static const String robotoItalic = "Roboto-Italic";
+  static const String robotoLight = "Roboto-Light";
+  static const String robotoLightItalic = "Roboto-LightItalic";
+  static const String robotoMedium = "Roboto-Medium";
+  static const String robotoMediumItalic = "Roboto-MediumItalic";
+  static const String robotoRegular = "Roboto-Regular";
+  static const String robotoThin = "Roboto-Thin";
+  static const String robotoThinItalic = "Roboto-ThinItalic";
+
+  // Font Inter
+  static const String interBlack = "Inter-Black";
+  static const String interBold = "Inter-Bold";
+  static const String interExtraBold = "Inter-ExtraBold";
+  static const String interExtraLight = "Inter-ExtraLight";
+  static const String interLight = "Inter-Light";
+  static const String interMedium = "Inter-Medium";
+  static const String interRegular = "Inter-Regular";
+  static const String interSemiBold = "Inter-SemiBold";
+  static const String interThin = "Inter-Thin";
+// static const String interLightItalic = "Inter-LightItalic";
+// static const String interMediumItalic = "Inter-MediumItalic";
+// static const String interThinItalic = "Inter-ThinItalic";
+}
--- lib/src/constants/style.dart
+++ lib/src/constants/style.dart
@@ -1,82 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
+import 'package:flutter_base/src/constants/font.dart';
 import 'color.dart';
 import 'dimens.dart';
-
 
 class CStyle {
   static double s14LH17_5 = 1.25;
   static double s12LH15 = 1.25;
 
   /// FontSize 32.0
-  static TextStyle headline1({TextStyle? style}) =>  TextStyle(
+  static TextStyle headline1({TextStyle? style}) => TextStyle(
+        fontFamily: AppFont.robotoRegular,
         color: CColor.textDark1,
         fontSize: CFontSize.headline1,
         fontWeight: FontWeight.w400,
       ).merge(style);
 
   /// FontSize 24.0
-  static TextStyle headline2({TextStyle? style}) =>  TextStyle(
+  static TextStyle headline2({TextStyle? style}) => TextStyle(
+        fontFamily: AppFont.robotoRegular,
         color: CColor.textDark1,
         fontSize: CFontSize.headline2,
         fontWeight: FontWeight.w400,
       ).merge(style);
 
   /// FontSize 20.0
-  static TextStyle headline3({TextStyle? style}) => builder(TextStyle(
+  static TextStyle headline3({TextStyle? style}) => TextStyle(
         color: CColor.textDark1,
         fontSize: CFontSize.headline3,
         fontWeight: FontWeight.w400,
-      ).merge(style));
+      ).merge(style);
 
   /// FontSize 16.0
-  static TextStyle headline4({TextStyle? style}) => builder( TextStyle(
+  static TextStyle headline4({TextStyle? style}) => TextStyle(
+        fontFamily: AppFont.robotoRegular,
         height: 1.25,
         color: CColor.textDark1,
         fontSize: CFontSize.headline4,
         fontWeight: FontWeight.w400,
         letterSpacing: .5,
-      ).merge(style));
+      ).merge(style);
 
   /// FontSize 14.0
-  static TextStyle paragraph1({TextStyle? style}) => builder(TextStyle(
+  static TextStyle paragraph1({TextStyle? style}) => TextStyle(
+        fontFamily: AppFont.robotoRegular,
         color: CColor.neutral1,
         fontSize: CFontSize.paragraph1,
-        height: 1.14285714286, // in css: Line height = 16
+        height: 1.14285714286,
+        // in css: Line height = 16
         fontWeight: FontWeight.w400,
-      ).merge(style));
+      ).merge(style);
 
   /// FontSize 13.0
-  static TextStyle paragraph2({TextStyle? style}) => builder(TextStyle(
+  static TextStyle paragraph2({TextStyle? style}) => TextStyle(
+        fontFamily: AppFont.robotoRegular,
         color: CColor.neutral1,
         fontSize: CFontSize.paragraph2,
         height: 1.25,
         fontWeight: FontWeight.w400,
-      ).merge(style));
+      ).merge(style);
 
   /// FontSize 12.0
-  static TextStyle paragraph3({TextStyle? style}) => builder(TextStyle(
+  static TextStyle paragraph3({TextStyle? style}) => TextStyle(
+        fontFamily: AppFont.robotoRegular,
         color: CColor.neutral1,
         fontSize: CFontSize.paragraph3,
         fontWeight: FontWeight.w400,
-      ).merge(style));
+      ).merge(style);
 
   /// FontSize 10.0
-  static TextStyle paragraph4({TextStyle? style}) => builder(TextStyle(
+  static TextStyle paragraph4({TextStyle? style}) => TextStyle(
+        fontFamily: AppFont.robotoRegular,
         color: CColor.neutral1,
         fontSize: CFontSize.paragraph4,
         fontWeight: FontWeight.w400,
-      ).merge(style));
+      ).merge(style);
 
   /// FontSize 8.0
-  static TextStyle paragraph5({TextStyle? style}) => builder(TextStyle(
+  static TextStyle paragraph5({TextStyle? style}) => TextStyle(
+        fontFamily: AppFont.robotoRegular,
         color: CColor.neutral1,
         fontSize: CFontSize.paragraph5,
         fontWeight: FontWeight.w400,
-      ).merge(style));
-
-  static TextStyle builder(TextStyle? style) => GoogleFonts.roboto(
-        textStyle: style,
-      );
+      ).merge(style);
 }
