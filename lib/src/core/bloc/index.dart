10f1b49a7737472e1c16596bf1cdabb17a518976
Đinh Văn Nam <itchange.nw@gmail.com>
2024-07-11 16:19:59 +0700

    Update code

---
+++ lib/src/core/bloc/index.dart
@@ -0,0 +1,5 @@
+export 'base_bloc.dart';
+export 'bloc_base_event.dart';
+export 'bloc_base_main.dart';
+export 'bloc_base_state.dart';
+export 'bloc_concurrency.dart';
--- lib/src/core/index.dart
+++ lib/src/core/index.dart
@@ -2,3 +2,4 @@ export 'domain/entities/entity.dart';
 export 'normalize.dart';
 export 'utils.dart';
 export 'parallel/index.dart';
+export 'bloc/index.dart';
