87e6f1d7d3892d3a4df7548227ab044357553e80
Đinh Văn Nam <itchange.nw@gmail.com>
2024-10-09 11:38:41 +0700

    Update code

--- lib/src/compoments/witem/witem_cupertino.dart
+++ lib/src/compoments/witem/witem_cupertino.dart
@@ -1,17 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_base/flutter_base.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class BaseItemWidget<T, F> extends StatefulWidget {
+class BaseCupertinoItemWidget<T, F> extends StatefulWidget {
   final T? item;
   final F? queryParameters;
   final Widget Function()? buildLoading;
   final List<Widget> Function(T item, BaseItemState<T> state) buildLoadedSlivers;
   final Widget Function(T item, Future Function() onRefresh)? buildCustomLoaded;
   final Widget Function(BaseItemState<T> state)? buildEmpty;
-  final SliverAppBar? sliverAppBar;
+  final CupertinoSliverNavigationBar? sliverAppBar;
+  final ObstructingPreferredSizeWidget? navigationBar;
 
-  const BaseItemWidget({
+  const BaseCupertinoItemWidget({
     super.key,
     this.item,
     this.queryParameters,
@@ -20,13 +22,14 @@ class BaseItemWidget<T, F> extends StatefulWidget {
     required this.buildLoadedSlivers,
     this.buildEmpty,
     this.sliverAppBar,
+    this.navigationBar,
   });
 
   @override
-  State<BaseItemWidget<T, F>> createState() => _BaseItemWidgetState<T, F>();
+  State<BaseCupertinoItemWidget<T, F>> createState() => _BaseCupertinoItemWidgetState<T, F>();
 }
 
-class _BaseItemWidgetState<T, F> extends State<BaseItemWidget<T, F>> {
+class _BaseCupertinoItemWidgetState<T, F> extends State<BaseCupertinoItemWidget<T, F>> {
   BaseItemBloc<T, F> get bloc => context.read<BaseItemBloc<T, F>>();
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
   F? queryParameters;
@@ -61,7 +64,7 @@ class _BaseItemWidgetState<T, F> extends State<BaseItemWidget<T, F>> {
   }
 
   @override
-  void didUpdateWidget(covariant BaseItemWidget<T, F> oldWidget) {
+  void didUpdateWidget(covariant BaseCupertinoItemWidget<T, F> oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (queryParameters != widget.queryParameters) {
       queryParameters = widget.queryParameters;
@@ -78,6 +81,11 @@ class _BaseItemWidgetState<T, F> extends State<BaseItemWidget<T, F>> {
             parent: AlwaysScrollableScrollPhysics(),
           ),
           slivers: [
+            if (widget.sliverAppBar != null) widget.sliverAppBar!,
+            CupertinoSliverRefreshControl(
+              key: _refreshIndicatorKey,
+              onRefresh: onRefresh,
+            ),
             SliverFillRemaining(
               child: Center(
                 child: AppErrorWidget(
@@ -96,18 +104,18 @@ class _BaseItemWidgetState<T, F> extends State<BaseItemWidget<T, F>> {
   Widget _buildLoadedSliver() {
     return BlocBuilder<BaseItemBloc<T, F>, BaseItemState<T>>(
       builder: (context, state) {
-        return RefreshIndicator(
-          key: _refreshIndicatorKey,
-          onRefresh: onRefresh,
-          child: CustomScrollView(
-            physics: const BouncingScrollPhysics(
-              parent: AlwaysScrollableScrollPhysics(),
-            ),
-            slivers: [
-              if (widget.sliverAppBar != null) widget.sliverAppBar!,
-              ...widget.buildLoadedSlivers(state.item as T, state),
-            ],
+        return CustomScrollView(
+          physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics(),
           ),
+          slivers: [
+            if (widget.sliverAppBar != null) widget.sliverAppBar!,
+            CupertinoSliverRefreshControl(
+              key: _refreshIndicatorKey,
+              onRefresh: onRefresh,
+            ),
+            ...widget.buildLoadedSlivers(state.item as T, state),
+          ],
         );
       },
     );
@@ -117,31 +125,40 @@ class _BaseItemWidgetState<T, F> extends State<BaseItemWidget<T, F>> {
   Widget build(BuildContext context) {
     return BlocBuilder<BaseItemBloc<T, F>, BaseItemState<T>>(
       builder: (context, state) {
-        switch (state.status) {
-          case EBlocStateStatus.loading:
-            {
-              return Center(child: widget.buildLoading?.call() ?? const CircularProgressIndicator());
-            }
+        return CupertinoPageScaffold(
+          navigationBar: widget.sliverAppBar != null ? null : widget.navigationBar,
+          child: SafeArea(
+            child: Builder(
+              builder: (context) {
+                switch (state.status) {
+                  case EBlocStateStatus.loading:
+                    {
+                      return Center(child: widget.buildLoading?.call() ?? const CupertinoActivityIndicator());
+                    }
 
-          case EBlocStateStatus.fail:
-            return _buildFailure();
+                  case EBlocStateStatus.fail:
+                    return _buildFailure();
 
-          case EBlocStateStatus.success:
-          case EBlocStateStatus.loadingMore:
-          case EBlocStateStatus.loadMoreFailure:
-          case EBlocStateStatus.loadMoreSuccess:
-          case EBlocStateStatus.loadingRefresh:
-          case EBlocStateStatus.loadRefreshFailure:
-          case EBlocStateStatus.loadRefreshSuccess:
-            if ((widget.buildEmpty != null && state.item == null)) {
-              return widget.buildEmpty!.call(state);
-            }
+                  case EBlocStateStatus.success:
+                  case EBlocStateStatus.loadingMore:
+                  case EBlocStateStatus.loadMoreFailure:
+                  case EBlocStateStatus.loadMoreSuccess:
+                  case EBlocStateStatus.loadingRefresh:
+                  case EBlocStateStatus.loadRefreshFailure:
+                  case EBlocStateStatus.loadRefreshSuccess:
+                    if ((widget.buildEmpty != null && state.item == null)) {
+                      return widget.buildEmpty!.call(state);
+                    }
 
-            return widget.buildCustomLoaded?.call(state.item!, onRefresh) ?? _buildLoadedSliver();
+                    return widget.buildCustomLoaded?.call(state.item!, onRefresh) ?? _buildLoadedSliver();
 
-          default:
-            return const SizedBox.shrink();
-        }
+                  default:
+                    return const SizedBox.shrink();
+                }
+              },
+            ),
+          ),
+        );
       },
     );
   }
